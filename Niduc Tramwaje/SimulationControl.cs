45bf83a6804f32e873e311374932b9d4e1becb24
Konrad Stręk <konrad.strek@gmail.com>
2020-04-01 02:39:44 +0200

    Klasa tram + podstawy

---
+++ Niduc Tramwaje/SimulationControl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Niduc_Tramwaje
+{
+    static class SimulationControl
+    {
+        static List<Tram> trams;
+        
+
+        static void Simulation() {
+
+        }
+
+        /*private class TramsOnTrack
+        {
+            private Dictionary<KeyValuePair<TramStop, TramStop>, List<KeyValuePair<Tram, float>>> tramsOnTrack;
+
+            public List<KeyValuePair<Tram,float>> GetTramsOnTrack(KeyValuePair<TramStop, TramStop> origindDestPair) {
+                return tramsOnTrack[origindDestPair];
+            }
+        }*/
+    }
+}
--- Niduc Tramwaje/Tram.cs
+++ Niduc Tramwaje/Tram.cs
@@ -8,5 +8,28 @@ namespace Niduc_Tramwaje
 {
     class Tram
     {
+        private float speed;
+        private Track track;
+        private bool forward;
+        private List<Passenger> passengers;
+        private TramStop currentTramStop;
+
+        Tram(float speed, Track track, TramStop currentTramStop) {
+            this.speed = speed;
+            this.track = track;
+            this.currentTramStop = currentTramStop;
+        }
+
+        public void Load() {
+
+        }
+
+        public void Unload() {
+
+        }
+
+        void GoTo(TramStop tramStop) {
+            //TODO
+        }
     }
 }
