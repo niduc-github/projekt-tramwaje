0f0c7a453c861877293fb2298a9a06c8469fd563
BlueCloud119 <markur911@gmail.com>
2020-04-20 21:19:15 +0200

    .

--- Niduc Tramwaje/TramStop.cs
+++ Niduc Tramwaje/TramStop.cs
@@ -10,16 +10,28 @@ namespace Niduc_Tramwaje
     class TramStop
     {
         private String name;
-        private Vector2 positiion;
+        private Vector2 position;
         private float amountOfPeople;
         private float popularity;
         List<Passenger> passengers;
 
         public void PassangerGeneration()
         {
-            if (popularity < 0.3) { }
-            if (popularity >= 0.3 && popularity < 0.7) { }
-            if (popularity >= 0.7) { }
+            if (popularity < 0.3) 
+            {
+                Random r = new Random();
+                amountOfPeople = r.Next(0, 10);
+            }
+            if (popularity >= 0.3 && popularity < 0.7) 
+            {
+                Random r = new Random();
+                amountOfPeople = r.Next(11, 30);
+            }
+            if (popularity >= 0.7) 
+            {
+                Random r = new Random();
+                amountOfPeople = r.Next(31, 60);
+            }
         }
 
         public float GetCurrentAmountOfPeople()
