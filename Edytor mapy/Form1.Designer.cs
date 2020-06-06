404df993c2b103ff3f2043999fc5c2364bbfa39b
Dominik Piątkowski <dominik.piatkowski.04@onet.pl>
2020-06-06 16:18:10 +0200

    .

--- Edytor mapy/Form1.Designer.cs
+++ Edytor mapy/Form1.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.picMap = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.btnDodajPrzystanek = new System.Windows.Forms.Button();
+            this.btnEdytujPrzystanek = new System.Windows.Forms.Button();
+            this.btnUsuńPrzystanek = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.picMap)).BeginInit();
             this.SuspendLayout();
             // 
@@ -50,11 +53,41 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "Test";
             // 
+            // btnDodajPrzystanek
+            // 
+            this.btnDodajPrzystanek.Location = new System.Drawing.Point(712, 68);
+            this.btnDodajPrzystanek.Name = "btnDodajPrzystanek";
+            this.btnDodajPrzystanek.Size = new System.Drawing.Size(125, 23);
+            this.btnDodajPrzystanek.TabIndex = 3;
+            this.btnDodajPrzystanek.Text = "Dodaj przystanek";
+            this.btnDodajPrzystanek.UseVisualStyleBackColor = true;
+            // 
+            // btnEdytujPrzystanek
+            // 
+            this.btnEdytujPrzystanek.Location = new System.Drawing.Point(712, 97);
+            this.btnEdytujPrzystanek.Name = "btnEdytujPrzystanek";
+            this.btnEdytujPrzystanek.Size = new System.Drawing.Size(125, 23);
+            this.btnEdytujPrzystanek.TabIndex = 3;
+            this.btnEdytujPrzystanek.Text = "Edytuj przystanek";
+            this.btnEdytujPrzystanek.UseVisualStyleBackColor = true;
+            // 
+            // btnUsuńPrzystanek
+            // 
+            this.btnUsuńPrzystanek.Location = new System.Drawing.Point(712, 126);
+            this.btnUsuńPrzystanek.Name = "btnUsuńPrzystanek";
+            this.btnUsuńPrzystanek.Size = new System.Drawing.Size(125, 23);
+            this.btnUsuńPrzystanek.TabIndex = 4;
+            this.btnUsuńPrzystanek.Text = "Usuń przystanek";
+            this.btnUsuńPrzystanek.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(902, 572);
+            this.Controls.Add(this.btnUsuńPrzystanek);
+            this.Controls.Add(this.btnEdytujPrzystanek);
+            this.Controls.Add(this.btnDodajPrzystanek);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.picMap);
             this.Name = "Form1";
@@ -69,6 +102,9 @@
 
         private System.Windows.Forms.PictureBox picMap;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDodajPrzystanek;
+        private System.Windows.Forms.Button btnEdytujPrzystanek;
+        private System.Windows.Forms.Button btnUsuńPrzystanek;
     }
 }
 
