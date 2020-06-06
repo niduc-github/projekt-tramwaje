5886efb4221daff4616139d8b42e67ba2b28e755
BlueCloud119 <markur911@gmail.com>
2020-06-06 16:36:09 +0200

    Przyciski

--- Edytor mapy/Form1.cs
+++ Edytor mapy/Form1.cs
@@ -16,15 +16,5 @@ namespace Edytor_mapy
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
